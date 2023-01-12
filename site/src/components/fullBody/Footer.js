ab2ddd4e351dfad578ba93206305e87add40e349
DimastaFromDonbas <117586881+DimastaFromDonbas@users.noreply.github.com>
2023-01-12 20:19:54 +0200

    add

--- site/src/components/fullBody/Footer.js
+++ site/src/components/fullBody/Footer.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import '../../style/footer.css'
 
 function Footer() {
@@ -8,7 +9,9 @@ function Footer() {
         <div class="container footer_holder">
             <p class="footer_copyright">© 2017– {year} Гарант сервис gamesgun.ru</p>
                 <ul class="footer-menu">
-                    <li class="footer-menu_item"><a class="footer-menu_link" href="/rules" target="_blank">Пользовательское соглашение</a></li>
+                    <li class="footer-menu_item">
+                        <Link class="footer-menu_link" to='#' target="_blank">Пользовательское соглашение</Link>
+                    </li>
                 </ul>
         </div>
         </div>
