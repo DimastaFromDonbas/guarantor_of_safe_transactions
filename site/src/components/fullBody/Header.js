be8a1b0f618c42669b41d015b375d5b0f6c4f3d5
DimastaFromDonbas <117586881+DimastaFromDonbas@users.noreply.github.com>
2023-01-12 21:17:12 +0200

    aadd

--- site/src/components/fullBody/Header.js
+++ site/src/components/fullBody/Header.js
@@ -1,7 +1,6 @@
-import { Navbar, Alert} from "react-bootstrap";
-import {Routes,Route,Link} from "react-router-dom"
+import { Alert} from "react-bootstrap";
+import {Link} from "react-router-dom"
 import '../../style/header.css'
-import img1 from '../../image/free-icon-home-7544449.png'
 import HomeIcon from '@mui/icons-material/Home';
 
 function Header() {
@@ -9,7 +8,7 @@ function Header() {
         <Alert.Heading style={{backgroundColor: '#E63F3F', color: 'white',margin: 0, height: "40px", textAlign: "center",fontSize: '17px',paddingBottom:'10px',paddingTop:'10px'}}>Технический чат поддержки работает с 10:00 до 20:00 ежедневно!</Alert.Heading>
               <div className="navbar-header">
               <div className="nav-main-header">
-              <Link className="color-nav-link color" to="/main"><HomeIcon/></Link>
+              <Link className="color-nav-link color" to="/main"><HomeIcon></HomeIcon></Link>
               <div className="flex-nav-link">
               <Link className="color-nav-link color" to="#">ПОМОЩЬ</Link>
               <Link className="color-nav-link color" to="#">РАЗРЕШЕНИЕ СПОРОВ</Link>
