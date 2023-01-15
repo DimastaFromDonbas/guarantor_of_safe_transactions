e77c2934ce0352e1f16f6d7b3b6481f76035ef10
DimastaFromDonbas <117586881+DimastaFromDonbas@users.noreply.github.com>
2023-01-15 16:12:31 +0200

    add redux file

---
+++ site/src/rdx/items/actions.js
@@ -0,0 +1,41 @@
+export const SET_COUNTRIES = '@items/SET_COUNTRIES';
+export const SET_PRODUCTS = '@items/SET_PRODUCTS';
+export const SET_CURRENCIES = '@items/SET_CURRENCIES';
+export const INCREASE_PRODUCT_VALUE = '@items/INCREASE_PRODUCT_VALUE';
+export const DECREASE_PRODUCT_VALUE = '@items/DECREASE_PRODUCT_VALUE';
+
+
+export const setCountries = (item) => {
+  return {
+    type: SET_COUNTRIES,
+    payload: item,
+  }
+}
+
+export const setProducts = (item) => {
+  return {
+    type: SET_PRODUCTS,
+    payload: item,
+  }
+}
+
+export const setAllCurrencies = (item) => {
+  return {
+    type: SET_CURRENCIES,
+    payload: item,
+  }
+}
+
+export const increaseProductValue = (item) => {
+  return {
+    type: INCREASE_PRODUCT_VALUE,
+    payload: item,
+  }
+}
+
+export const decreaseProductValue = (item) => {
+  return {
+    type: DECREASE_PRODUCT_VALUE,
+    payload: item,
+  }
+}
---
+++ site/src/rdx/items/selectors.js
@@ -0,0 +1,3 @@
+export const selectAllCountries = (state) => state.items.countries;
+export const selectAllProducts = (state) => state.items.products;
+export const selectAllCurrencies = (state) => state.items.currency;
