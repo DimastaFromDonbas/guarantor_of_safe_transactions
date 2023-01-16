a28458f2d35b907efbcc8686a88e3afe9eb5f382
DimastaFromDonbas <117586881+DimastaFromDonbas@users.noreply.github.com>
2023-01-16 20:29:30 +0200

    fix

--- site/src/components/fullBody/Header.js
+++ site/src/components/fullBody/Header.js
@@ -35,9 +35,9 @@ function Header() {
         <Alert.Heading style={{backgroundColor: '#E63F3F', color: 'white',margin: 0, height: "40px", textAlign: "center",fontSize: '17px',paddingBottom:'10px',paddingTop:'10px'}}>Технический чат поддержки работает с 10:00 до 20:00 ежедневно!</Alert.Heading>
               <div className="navbar-header">
               <div className="nav-main-header">
-                <Link className="color-nav-link color" to="/"><HomeIcon style={{marginLeft: "17px"}}></HomeIcon></Link>
+                <Link className="color-nav-link color" to="/"><HomeIcon></HomeIcon></Link>
               { user?.id? 
-                <div style={{ marginLeft: "75px",display: "flex",width: "600px",justifyContent: "space-between",alignItems: "center"}} >
+                <div style={{ marginLeft: "25px",display: "flex",width: "600px",justifyContent: "space-between",alignItems: "center"}} >
                 <Link className="color-nav-link color" to="#">МОИ СДЕЛКИ</Link>
                 <Link className="color-nav-link color" to="#">МОЙ СЧЕТ:{user.score} РУБ.</Link>
                 <Link className="color-nav-link color" to="#">ПОМОЩЬ</Link>
@@ -53,18 +53,15 @@ function Header() {
               </div>
               { user?.id?
                 <div style={{display: "flex",justifyContent: "space-between"}}>
-                <Link className="color-nav-link color" to="/login" style={{display: 'flex', flexDirection: 'row'}}>{user.nickname}</Link>
-                <KeyboardArrowDownIcon onBlur = {clickArrowdown} onClick = {clickArrowdown} className="hoverArrow"></KeyboardArrowDownIcon>
-                  { checked?
-                  <div className="user-profile-block js-profile-block_open ">
+                <Link className="color-nav-link color" to="#" style={{display: 'flex', flexDirection: 'row'}}>{user.nickname}</Link>
+                <KeyboardArrowDownIcon  onClick = {clickArrowdown} className="hoverArrow"></KeyboardArrowDownIcon>
+                  <div className={checked?"user-profile-block js-profile-block_open active": "user-profile-block js-profile-block_open"}>
                         <ul className="nav-detail_list">
                             <li className="nav-detail_item"><Link className="nav-detail_link" to="/systemmessages">Системные сообщения</Link></li>
                               <li className="nav-detail_item"><Link className="nav-detail_link" to="/settings">Мои настройки</Link></li>
-                             <li className="nav-detail_item"><Link onClick={getUsers} className="nav-detail_link" to="/">Выход</Link></li>
+                             <li className="nav-detail_item border-exit"><Link onClick={getUsers} className="nav-detail_link" to="/">Выход</Link></li>
                            </ul>
                     </div>
-                    :
-                    ''}
                 </div>
                 :
                 <div className="flex-nav-link-registr ">
