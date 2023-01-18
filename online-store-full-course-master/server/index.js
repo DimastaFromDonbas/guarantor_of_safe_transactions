fe6d276297b46345f3de7dde7830113f29d68ff7
Alezhaw <oleksandr.baiev@nure.ua>
2023-01-18 18:44:05 +0200

    начало бэка для чатов

--- online-store-full-course-master/server/index.js
+++ online-store-full-course-master/server/index.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
@@ -11,12 +13,19 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const app = express()
-app.use(cors())
+app.use(cors({ origin: '*'}))
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
 
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    }
+})
 // Обработка ошибок, последний Middleware
 app.use(errorHandler)
 
@@ -24,7 +33,7 @@ const start = async () => {
     try {
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+        server.listen(PORT, () => console.log(`Server started on port ${PORT}`))
     } catch (e) {
         console.log(e)
     }
