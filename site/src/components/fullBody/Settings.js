60d1159cdc9e03090fa4f253a5a4af5edaaafaa3
DimastaFromDonbas <117586881+DimastaFromDonbas@users.noreply.github.com>
2023-01-21 14:58:23 +0200

    setting all fix

--- site/src/components/fullBody/Settings.js
+++ site/src/components/fullBody/Settings.js
@@ -3,10 +3,11 @@ import Footer from "./Footer"
 import Header from "./Header"
 import { useDispatch } from 'react-redux';
 import { reducerTypes } from "../../store/Users/types";
-import { axiosChangeNickname } from "../../api/axios";
+import { axiosChangeNickname, axiosChangePassword } from "../../api/axios";
 import { useEffect, useState } from "react";
 import { useAppSelector } from "../../store/reduxHooks";
 import HeadsetMicIcon from '@mui/icons-material/HeadsetMic';
+import validator from 'validator';
 
 function Settings() {
 
@@ -14,7 +15,13 @@ function Settings() {
     const {user} = useAppSelector ((store) => store.user)
     const [nickname, setNickname] = useState("");
     const [password, setPassword] = useState("");
+    const [passwordErorr,setPasswordErorr] = useState("");
     const [formValid, setFormValid] = useState(false);
+    const [passwordV2, setPasswordV2] = useState("");
+    const [formValidPassword, setFormValidPassword] = useState(false);
+    const [newPassword, setNewPassword] = useState('')
+    const [passwordError, setPasswordError] = useState("");
+    const [NewPasswordError, setNewPasswordError] = useState("");
 
     function changeNickname(e) {
         setNickname(e.currentTarget.value)
@@ -22,6 +29,33 @@ function Settings() {
 
     function getPassword(e) {
         setPassword(e.currentTarget.value)
+        if(user.password !== e.currentTarget.value) {
+            setPasswordErorr('Проверьте введенные данные')
+        } else {
+            setPasswordErorr('')
+    }
+    }
+
+    function getPasswordV2(e) {
+        setPasswordV2(e.currentTarget.value)
+        if(user.password !== e.currentTarget.value) {
+            setPasswordError('Проверьте введенные данные')
+        } else {
+      setPasswordError('')
+    }
+    }
+
+    function getNewPasswordOnChange(e) {
+        setNewPassword(e.currentTarget.value)
+        if(!validator.isStrongPassword(e.currentTarget.value, {minSymbols: 0,minUppercase: 0})) {
+            setNewPasswordError( 'Минимальная длина 8' )
+            if(!e.currentTarget.value){
+                setNewPasswordError('Пароль не может быть пустым')
+            }
+          } else {
+            setNewPasswordError('')
+          }
+
     }
 
     async function getNicknames(e) {
@@ -29,6 +63,14 @@ function Settings() {
         dispatch({
           type: reducerTypes.GET_USER,
           payload: await axiosChangeNickname(nickname, user.id, user.password)
+        });
+    }
+
+    async function getNewPassword(e) {
+        e.preventDefault()
+        dispatch({
+          type: reducerTypes.GET_USER,
+          payload: await axiosChangePassword(newPassword, user.id, user.password)
         });
     }
 
@@ -42,26 +84,55 @@ function Settings() {
         }
       },[nickname, password, user.password])
 
+      useEffect(() => {
+        if(user.password === passwordV2) {
+            if(newPassword !== ''){
+                if(NewPasswordError === '') {
+                    setFormValidPassword(true)
+                }
+            }
+        } else {
+            setFormValidPassword(false)
+        }
+      },[newPassword, passwordV2, user.password,NewPasswordError])
+
     return <div className="bg-img">
         <Header />
         <HeadsetMicIcon className='chat-icon icon-chat-sizes'></HeadsetMicIcon>
             <div style={{paddingBottom: "117px", minHeight: "calc(100vh - 339px)"}} className="wraper">
                 <h3 className="header-inner_title login-inner_title">Настройки</h3>
                     <hr className="hr-viss"/>
-                    <Form className="width-form">
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label className="color-input-name">Сменить ваше имя:</Form.Label>
-                            <Form.Control value={nickname} onChange={changeNickname} name='login' type="email" placeholder="" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label className="color-input-name">Введите ваш пароль:</Form.Label>
-                            <Form.Control value={password} onChange={getPassword} name='password' type="password" placeholder="" />
-                            {user.password!==password? <div style={{color: 'red'}}>Проверьте пароль, вы ошиблись</div> : ''}
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        </Form.Group>
-                        <button disabled={!formValid} onClick={(e) => getNicknames(e)} className="btn-class-v2">Сменить ник</button>
-                    </Form>
+                    <div style={{display: 'flex'}}>
+                        <Form className="width-form">
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label className="color-input-name">Сменить ваше имя:</Form.Label>
+                                <Form.Control value={nickname} onChange={changeNickname} name='login' type="email" placeholder="" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label className="color-input-name">Введите ваш пароль:</Form.Label>
+                                <Form.Control value={password} onChange={getPassword} name='password' type="password" placeholder="" />
+                                {passwordErorr? <div style={{color: 'red'}}>{passwordErorr}</div> : ''}
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            </Form.Group>
+                            <button disabled={!formValid} onClick={(e) => getNicknames(e)} className="btn-class-v2">Сменить ник</button>
+                        </Form>
+                        <Form className="width-form">
+                            <Form.Group className="mb-3" controlId="formBasicEmailV2">
+                            <Form.Label className="color-input-name">Сменить ваш пароль:</Form.Label>
+                                <Form.Control value={newPassword} onChange={getNewPasswordOnChange} name='login' type="password" placeholder="" />
+                                {NewPasswordError? <div style={{color: 'red'}}>{NewPasswordError}</div> : ''}
+                           </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPasswordV2">
+                                <Form.Label className="color-input-name">Введите ваш пароль:</Form.Label>
+                                <Form.Control value={passwordV2} onChange={getPasswordV2} name='password' type="password" placeholder="" />
+                                {passwordError? <div style={{color: 'red'}}>{passwordError}</div> : ''}
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            </Form.Group>
+                            <button style={{width: '135px'}} disabled={!formValidPassword} onClick={(e) => getNewPassword(e)} className="btn-class-v2">Сменить пароль</button>
+                        </Form>
+                    </div>
             </div>
         <Footer />
     </div>
