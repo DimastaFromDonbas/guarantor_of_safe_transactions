e45e565bf843d12eac59ad22e259531b7e04f786
Alezhaw <oleksandr.baiev@nure.ua>
2023-01-22 16:16:38 +0200

    fix linearProgress

--- site/src/components/fullBody/Deal.js
+++ site/src/components/fullBody/Deal.js
@@ -1,25 +1,16 @@
 import Footer from "./Footer";
 import Header from "./Header";
-import PropTypes from 'prop-types';
 import LinearProgress from '@mui/material/LinearProgress';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { useEffect, useState } from "react";
 
 function Deal() { 
+    const [progress, setProgress] = useState(50)
+
     return <div className="bg-img">
         <Header />
             <div className='container wrap'>
                 <div className="message-body">
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Box sx={{ width: '100%', mr: 1 }}>
-                            <LinearProgress variant="determinate" {...props} />
-                        </Box>
-                        <Box sx={{ minWidth: 35 }}>
-                            <Typography variant="body2" color="text.secondary">{`${Math.round(
-                            props.value,
-                            )}%`}</Typography>
-                        </Box>
-                    </Box>
+                <LinearProgress variant="determinate" value={progress} />
                 </div>
             </div>
         <Footer />
