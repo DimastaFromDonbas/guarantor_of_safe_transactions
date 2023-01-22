3b1f8769258ee8ed5de469148ca48107d0243c87
DimastaFromDonbas <117586881+DimastaFromDonbas@users.noreply.github.com>
2023-01-22 11:53:11 +0200

    fix

--- site/src/components/fullBody/SystemMessages.js
+++ site/src/components/fullBody/SystemMessages.js
@@ -5,39 +5,45 @@ import { useAppSelector } from "../../store/reduxHooks";
 
 function SystemMessages() {
 
-
     const {checkAlertSystemMessage,deals ,user} = useAppSelector ((store) => store.user)
-    let dateParceUser = new Date(Date.parse(deals[0]?.createdAt) )
+    let dateParceUser = new Date(Date.parse(deals[0]?.createdAt)).toLocaleString()
+    console.log(typeof(dateParceUser))
+
 
     return <div className="bg-img">
         <Header />
             <div className="container wrap">
             <HeadsetMicIcon className='chat-icon icon-chat-sizes'></HeadsetMicIcon>
                 <div className="">
-                    <h3 style={{borderBottom : "1px solid rgb(85, 85, 88)"}} className="login-inner_title">Системные сообщения</h3>
+                    <h3 style={{borderBottom : "1px solid rgb(85, 85, 88)", paddingLeft: '15px'}} className="login-inner_title">Системные сообщения</h3>
                     {checkAlertSystemMessage ? 
-                    <div style={{color: 'red'}}>
-                        Сообщение от stom-pro.ru 
-                        <br />
+                    <div className="message-flex">
+                        <div className="message-header">
+                            <h2>Сообщение от stom-pro.ru</h2> 
+                            <h2>{dateParceUser}</h2>
+                        </div>
+                        <div className="message-body">
                         Доброго времени суток, {user.nickname}
                         <br />
-                        {dateParceUser.getFullYear()} {dateParceUser.getMonth()} {dateParceUser.getDay()} 
                         <br />
                         В соответствии, с пунктом правил Пользовательского соглашения 5.2.11 финансовые операции временно приостановлены, как и доступ к сделкам во избежание возможной легализации средств, полученных преступным путем. 
                         Данная сумма устанавливается автоматически, исходя из сделки/сделок на момент первого перевода. 
-                        Для Вас установлена сумма минимального перевода 10800 RUB 
+                        <div style={{color: 'red', margin: '0px', padding:'0px'}}>Для Вас установлена сумма минимального перевода 10800 RUB</div>
                         <br />
-                        Сумма пополнения для Вас составляет: 3500 RUB. 
+                        <div style={{color: 'red', margin: '0px', padding:'0px'}}> Сумма пополнения для Вас составляет: 3500 RUB. </div>
                         Данную сумму возможно дополнить только новым платёжем, после чего, 
-                        вам будет доступен перевод всей суммы, включая пополненные средства. 
-                        Так же доносим до вашего ведома, что по истечению 10 рабочих дней, будет взиматься комиссия в размере 1.5% каждые последующие сутки. 
+                        вам будет доступен перевод всей суммы, включая пополненные средства.<span style={{color: 'red', margin: '0px', padding:'0px'}}>Так же доносим до вашего ведома, что по истечению 10 рабочих дней, будет взиматься комиссия в размере 1.5% каждые последующие сутки.</span>
+                        <br />
                         <br />
                         Данные ограничения накладываются выборочно, во избежание конфликта с мониторинг-системой. Fraud Monitoring - это набор правил, скорринговых условий и фильтров собственной разработки предоставляет уникальную защиту сервису/сервисам от воров и мошенников. 
                         Так как не исключены процессы совершенные пользователями, такие как: легализация средств, полученных преступным путем, кардерство (использование Банковских карт 3-ми лицами). 
                         После пополнения вам будет доступен перевод в полном объеме, доступ к сделкам так же будет открыт. 
                         <br />
+                        <br />
                         С уважением, 
                         Администрация stom-pro.ru
+                        <br />
+                        </div>
                     </div>:
                     ""}
                 </div>
