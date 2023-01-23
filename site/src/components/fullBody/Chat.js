a9b2656abab04e34532b63ce1d22afb1996b7c7d
DimastaFromDonbas <117586881+DimastaFromDonbas@users.noreply.github.com>
2023-01-23 14:56:49 +0200

    Update Chat.js

--- site/src/components/fullBody/Chat.js
+++ site/src/components/fullBody/Chat.js
@@ -6,22 +6,22 @@ function Chat() {
 
     const [checked, setChecked] = useState(false)
 
-    return <>
+    return  <>
                 {checked?
-                <div className='chat activ-chat'>
-                <div className='chat-header'>
-                    <img alt='img-helper' className='img-helper' src={Helper}></img>
-                    <div className='helper-nickName'>
-                        <div style={{fontWeight: "bold"}}>Служба поддержки</div>
-                        <div style={{fontSize: '12px'}}>Онлайн</div>
-                    </div>
-                </div>
-                <div></div>
-                <div></div>
-            </div>:
-            ''}
-            <HeadsetMicIcon onClick={() => setChecked(!checked)} className='chat-icon icon-chat-sizes'></HeadsetMicIcon>
-    </>
+                    <div className='chat activ-chat'>
+                        <div className='chat-header'>
+                            <img alt='img-helper' className='img-helper' src={Helper}></img>
+                            <div className='helper-nickName'>
+                                <div style={{fontWeight: "bold"}}>Служба поддержки</div>
+                                <div style={{fontSize: '12px'}}>Онлайн</div>
+                            </div>
+                        </div>
+                        <div></div>
+                        <div></div>
+                    </div>:
+                ''}
+                <HeadsetMicIcon onClick={() => setChecked(!checked)} className='chat-icon icon-chat-sizes'></HeadsetMicIcon>
+            </>
 }
 
 export default Chat
