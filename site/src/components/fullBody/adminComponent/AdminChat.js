1e5418a5b198a21d5969771288169bf5f30133ca
DimastaFromDonbas <117586881+DimastaFromDonbas@users.noreply.github.com>
2023-02-12 13:37:53 +0200

    fix

--- site/src/components/fullBody/adminComponent/AdminChat.js
+++ site/src/components/fullBody/adminComponent/AdminChat.js
@@ -1,17 +1,17 @@
-import { useEffect, useState, useRef } from 'react';
-import { useDispatch } from 'react-redux';
-import { useNavigate, useParams } from 'react-router-dom';
-import { axiosGetAdminChats, axiosGetMessagestoAdmin } from '../../../api/axios';
-import { useAppSelector } from '../../../store/reduxHooks';
-import { reducerTypes } from '../../../store/Users/types';
-import { socketAdmin } from '../AdminPanel';
-import { adminChatStatusMock, adminChatNewMessageMock } from '../../mock/OutputMock';
+import {useEffect, useState, useRef} from 'react';
+import {useDispatch} from 'react-redux';
+import {useNavigate, useParams} from 'react-router-dom';
+import {axiosGetAdminChats, axiosGetMessagestoAdmin} from '../../../api/axios';
+import {useAppSelector} from '../../../store/reduxHooks';
+import {reducerTypes} from '../../../store/Users/types';
+import {socketAdmin} from '../AdminPanel';
+import {adminChatStatusMock, adminChatNewMessageMock} from '../../mock/OutputMock';
 
 function AdminChat() {
-    const { email } = useParams();
+    const {email} = useParams();
     const dispatch = useDispatch();
     const statebackground = !!localStorage.getItem('backroundImg');
-    const { adminChat, user, adminMessage } = useAppSelector((store) => store.user);
+    const {adminChat, user, adminMessage} = useAppSelector((store) => store.user);
     const [statusForUserChat, setStatusForUserChat] = useState(1);
     const [currentChat, setCurrentChat] = useState(null);
     const [message, setMessage] = useState('');
@@ -59,7 +59,7 @@ function AdminChat() {
         const temporaryChat = adminChat?.filter((item) => item.email === email)[0];
         dispatch({
             type: reducerTypes.GET_ADMIN_MESSAGE,
-            payload: [...adminMessage, { ...temporaryChat, newMessage: 2 }]
+            payload: [...adminMessage, {...temporaryChat, newMessage: 2}]
         });
     }
 
@@ -85,7 +85,7 @@ function AdminChat() {
 
     useEffect(() => {
         if (currentChat?.email) {
-            socketAdmin.emit('join', { name: currentChat?.email, room: currentChat?.email });
+            socketAdmin.emit('join', {name: currentChat?.email, room: currentChat?.email});
         }
     }, [user, currentChat]);
 
@@ -100,7 +100,7 @@ function AdminChat() {
     }, [currentChat]);
 
     useEffect(() => {
-        socketAdmin.on('messageToAdmin', ({ data }) => {
+        socketAdmin.on('messageToAdmin', ({data}) => {
             if (adminMessage?.includes(data)) return;
             dispatch({
                 type: reducerTypes.GET_ADMIN_MESSAGE,
@@ -111,7 +111,7 @@ function AdminChat() {
     }, [adminMessage]);
 
     useEffect(() => {
-        socketAdmin.on('updateChatStatus', ({ data }) => {
+        socketAdmin.on('updateChatStatus', ({data}) => {
             if (data) {
                 alert('Успешно удалено');
             }
@@ -129,47 +129,47 @@ function AdminChat() {
     return (
         <>
             <div
-                style={{ display: 'flex', minHeight: '100vh', justifyContent: 'center' }}
+                style={{display: 'flex', minHeight: '100vh', justifyContent: 'center'}}
                 className={!statebackground ? 'styleAdminPanel' : 'styleAdminPanel2'}
             >
-                <div style={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', background: 'rgba(17, 17, 18, 0.65)' }}>
-                    <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', marginTop: '10px', color: 'white' }}>
+                <div style={{width: '100%', display: 'flex', flexDirection: 'column', alignItems: 'center', background: 'rgba(17, 17, 18, 0.65)'}}>
+                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', marginTop: '10px', color: 'white'}}>
                         <div onClick={() => navigate('/adminPanel')} className="tabl-flex-admin-button-global2">
                             Вернуться назад
                         </div>
                     </div>
 
-                    <div style={{ marginTop: '20px', color: 'white' }}>
-                        <div style={{ borderRadius: '5px' }} className="tabl-flex-admin">
-                            <div style={{ textAlign: 'center', width: '50px' }} className="output-id">
+                    <div style={{marginTop: '20px', color: 'white'}}>
+                        <div style={{borderRadius: '5px'}} className="tabl-flex-admin">
+                            <div style={{textAlign: 'center', width: '50px'}} className="output-id">
                                 ID
                             </div>
-                            <div style={{ textAlign: 'center', width: '155px' }} className="output-sum">
+                            <div style={{textAlign: 'center', width: '155px'}} className="output-sum">
                                 Имя пользователя
                             </div>
-                            <div style={{ textAlign: 'center', width: '210px' }} className="output-sum">
+                            <div style={{textAlign: 'center', width: '210px'}} className="output-sum">
                                 Почта пользователя
                             </div>
-                            <div style={{ textAlign: 'center', width: '155px' }} className="output-sum">
+                            <div style={{textAlign: 'center', width: '155px'}} className="output-sum">
                                 Статус для пользователя
                             </div>
-                            <div style={{ textAlign: 'center', width: '155px' }} className="output-sum">
+                            <div style={{textAlign: 'center', width: '155px'}} className="output-sum">
                                 Время прошлого удаления чата
                             </div>
-                            <div style={{ textAlign: 'center', width: '120px' }} className="output-date">
+                            <div style={{textAlign: 'center', width: '120px'}} className="output-date">
                                 Оценка
                             </div>
                         </div>
                         {
-                            <div style={{ marginTop: '5px', borderRadius: '5px' }} className="tabl-flex-admin-user" key={currentChat?.email}>
+                            <div style={{marginTop: '5px', borderRadius: '5px'}} className="tabl-flex-admin-user" key={currentChat?.email}>
                                 <div
-                                    style={{ width: '50px', minHeight: '48px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                                    style={{width: '50px', minHeight: '48px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}
                                     className="output-id"
                                 >
                                     {currentChat?.id}
                                 </div>
                                 <div
-                                    style={{ width: '155px', minHeight: '48px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                                    style={{width: '155px', minHeight: '48px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}
                                     className="output-sum"
                                 >
                                     {currentChat?.nickname}
@@ -188,19 +188,19 @@ function AdminChat() {
                                     {currentChat?.email}
                                 </div>
                                 <div
-                                    style={{ width: '155px', minHeight: '48px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                                    style={{width: '155px', minHeight: '48px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}
                                     className="output-sum"
                                 >
                                     {adminChatStatusMock[statusForUserChat - 1]}
                                 </div>
                                 <div
-                                    style={{ width: '155px', minHeight: '48px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                                    style={{width: '155px', minHeight: '48px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}
                                     className="output-sum"
                                 >
                                     {currentChat?.deleteChatTime}
                                 </div>
                                 <div
-                                    style={{ width: '120px', minHeight: '48px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                                    style={{width: '120px', minHeight: '48px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}
                                     className="output-date"
                                 >
                                     {currentChat?.rate}
@@ -208,11 +208,11 @@ function AdminChat() {
                             </div>
                         }
                         <div className="pages-user-box-2">
-                            <div style={{ flexDirection: 'column', alignItems: 'center' }} className="pages-user-block">
-                                <h6 style={{ margin: '0', textAlign: 'center' }}>Изменение статуса для пользователя</h6>
+                            <div style={{flexDirection: 'column', alignItems: 'center'}} className="pages-user-block">
+                                <h6 style={{margin: '0', textAlign: 'center'}}>Изменение статуса для пользователя</h6>
                                 <select
                                     onChange={(e) => setStatusForUserChat(Number(e.target.value))}
-                                    style={{ color: 'white', borderRadius: '5px' }}
+                                    style={{color: 'white', borderRadius: '5px'}}
                                     className="tabl-flex-admin-user-scores "
                                     name="select"
                                     value={String(statusForUserChat || 1)}
@@ -228,32 +228,32 @@ function AdminChat() {
                     </div>
 
                     <div className="blockMassegesAdminPanel">
-                        <div style={{ display: 'flex', justifyContent: 'center', background: 'rgba(90, 89, 89, 0.75)' }}>
+                        <div style={{display: 'flex', justifyContent: 'center', background: 'rgba(90, 89, 89, 0.75)'}}>
                             <h2>Чат с {currentChat?.nickname}</h2>
                         </div>
 
-                        <div style={{ display: 'flex', flexDirection: 'column', overflow: 'overlay', height: '688px', paddingBottom: '40px' }} ref={chatRef}>
+                        <div style={{display: 'flex', flexDirection: 'column', overflow: 'overlay', maxHeight: '80vh'}} ref={chatRef}>
                             {adminMessage
                                 ?.filter((item) => !!item.message)
                                 ?.map((item) => (
                                     <div key={item?.id}>
                                         {item?.role === 'USER' ? (
                                             <div className="massegeStyleUserChat">
-                                                <p>
+                                                <p style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
                                                     {item?.nickname}: {item?.message} <span className="posMassegeses">{item?.time}</span>
                                                 </p>
                                             </div>
                                         ) : (
                                             <div className="massegeStyleAdminChat">
                                                 <p style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-end'}}>
-                                                    {item?.message}: {item?.administratorName} <span className="posMassegeses">{item?.time}</span>
+                                                    {item?.message} : {item?.administratorName} <span className="posMassegeses">{item?.time}</span>
                                                 </p>
                                             </div>
                                         )}
                                     </div>
                                 ))}
                         </div>
-                        <div style={{ position: 'absolute', bottom: '0px', width: '100%' }}>
+                        <div style={{position: 'absolute', bottom: '0px', width: '100%'}}>
                             <input
                                 className="tabl-flex-admin-masseges"
                                 type="text"
