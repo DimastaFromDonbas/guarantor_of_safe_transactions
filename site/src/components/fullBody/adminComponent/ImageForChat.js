f927979150d370b6b69d90f792167ca2b0a8e841
DimastaFromDonbas <117586881+DimastaFromDonbas@users.noreply.github.com>
2023-02-13 12:32:31 +0200

    fix

--- site/src/components/fullBody/adminComponent/ImageForChat.js
+++ site/src/components/fullBody/adminComponent/ImageForChat.js
@@ -7,7 +7,7 @@ export function ImageModal({ src }) {
         <>
             <img onClick={() => setOpen(true)} src={src} />
             {open && (
-                <div onClick={() => setOpen(false)} style={{ position: "fixed", top: 0, left: 0, right: 0, bottom: 0, background: "rgba(0, 0, 0, 0.5)" }}>
+                <div onClick={() => setOpen(false)} style={{ position: "fixed", top: 0, left: 0, right: 0, bottom: 0, background: "rgba(0, 0, 0, 0.5)" ,backdropFilter: "blur(5.5px)"}}>
                     <img style={{ maxHeight: "100%", maxWidth: "100%", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }} src={src} />
                 </div>
             )}
