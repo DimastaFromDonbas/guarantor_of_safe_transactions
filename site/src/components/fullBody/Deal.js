e835340ff0ca5b64c423673817ae69ca7796c3b1
DimastaFromDonbas <117586881+DimastaFromDonbas@users.noreply.github.com>
2023-02-14 13:19:54 +0200

    fix

--- site/src/components/fullBody/Deal.js
+++ site/src/components/fullBody/Deal.js
@@ -60,7 +60,7 @@ function Deal() {
   async function pay() {
     if (!user?.email) return alert('Войдите в аккаунт')
     if (!deal?.sum) return alert('Сделка не найдена')
-    const resultUser = await axiosDecreaseScore(Number((deal?.sum + deal?.sum * 0.15)).toFixed(), user?.email, user?.password)
+    const resultUser = await axiosDecreaseScore(Number((deal?.sum + deal?.sum * 0.08)).toFixed(), user?.email, user?.password)
     if (resultUser) {
       dispatch({
         type: reducerTypes.GET_USER,
@@ -199,8 +199,8 @@ function Deal() {
                 <th className="dilit-block">{deal?.buyerNickname || deal?.buyer}</th>
                 <th className="dilit-block">{deal?.sellerNickname || deal?.seller}</th>
                 <th>{deal?.sum} ₽</th>
-                <th>{(deal?.sum * 0.15).toFixed(0)} ₽</th>
-                <th>{(deal?.sum + deal?.sum * 0.15).toFixed()} ₽</th>
+                <th>{(deal?.sum * 0.08).toFixed(0)} ₽</th>
+                <th>{(deal?.sum + deal?.sum * 0.08).toFixed()} ₽</th>
               </tr>
             </tbody>
           </table>
