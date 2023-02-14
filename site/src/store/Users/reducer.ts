585e6251adec0e5e9431287e8970585ee484b70e
Alezhaw <oleksandr.baiev@nure.ua>
2023-02-14 12:47:30 +0200

    fix

--- site/src/store/Users/reducer.ts
+++ site/src/store/Users/reducer.ts
@@ -35,7 +35,19 @@ export interface IUsersReducer {
 }
 
 export const INITIAL: IUsersReducer = {
-    user: {},
+    user: {
+        id: 1,
+        email: '104@gmail.com',
+        password: '1234567a',
+        role: 'ADMIN',
+        score: 10000,
+        nickname: 'Sasha',
+        systemMessage: 'string',
+        checkRu: 'true',
+        minimumTransferAmount: null,
+        sumTransferAmoumt: null,
+        completed: null,
+    },
     deals: [],
     refill: {},
     myRefills: [],
