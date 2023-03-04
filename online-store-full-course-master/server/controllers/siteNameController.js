f6df6edeae078bcc3059d314d7be80b72087150b
Alezhaw <oleksandr.baiev@nure.ua>
2023-03-04 18:33:34 +0200

    test fix

--- online-store-full-course-master/server/controllers/siteNameController.js
+++ online-store-full-course-master/server/controllers/siteNameController.js
@@ -1,14 +1,14 @@
 const ApiError = require('../error/ApiError');
-const {User, SiteName} = require('../models/models')
+const { User, SiteName } = require('../models/models')
 const bcrypt = require('bcrypt')
 
 class SiteNameController {
     async create(req, res, next) {
-        const {name, email, password} = req.body
-        if(!name || !email || !password) {
+        const { name, email, password } = req.body
+        if (!name || !email || !password) {
             return next(ApiError.internal('Введите все данные'))
         }
-        const creator = await User.findOne({where: {email}})
+        const creator = await User.findOne({ where: { email } })
         if (!creator) {
             return next(ApiError.internal('Админ не найден'))
         }
@@ -16,19 +16,20 @@ class SiteNameController {
         if (!comparePassword) {
             return next(ApiError.internal('Указан неверный пароль'))
         }
-        if(creator.role !== 'ADMIN'){
+        if (creator.role !== 'ADMIN') {
             return next(ApiError.badRequest('Нет доступа'))
         }
-           const siteName = await SiteName.create({siteName: name})
-           return res.json(siteName)
+        const siteName = await SiteName.create({ siteName: name })
+        return res.json(siteName)
     }
 
     async update(req, res, next) {
-        const {name, email, password} = req.body
-        if(!name || !email || !password) {
+        const { name, email, password } = req.body
+        console.log('name', name, email, password)
+        if (!name || !email || !password) {
             return next(ApiError.internal('Введите все данные'))
         }
-        const creator = await User.findOne({where: {email}})
+        const creator = await User.findOne({ where: { email } })
         if (!creator) {
             return next(ApiError.internal('Админ не найден'))
         }
@@ -36,19 +37,19 @@ class SiteNameController {
         if (!comparePassword) {
             return next(ApiError.internal('Указан неверный пароль'))
         }
-        if(creator.role !== 'ADMIN'){
+        if (creator.role !== 'ADMIN') {
             return next(ApiError.badRequest('Нет доступа'))
         }
-           const siteName = await SiteName.update({siteName: name}, {where: {id: 1}})
-           return res.json(name)
+        const siteName = await SiteName.update({ siteName: name }, { where: { id: 1 } })
+        return res.json(name)
     }
 
     async updateWallet(req, res, next) {
-        const {wallet, email, password} = req.body
-        if(!wallet || !email || !password) {
+        const { wallet, email, password } = req.body
+        if (!wallet || !email || !password) {
             return next(ApiError.internal('Введите все данные'))
         }
-        const creator = await User.findOne({where: {email}})
+        const creator = await User.findOne({ where: { email } })
         if (!creator) {
             return next(ApiError.internal('Админ не найден'))
         }
@@ -56,25 +57,25 @@ class SiteNameController {
         if (!comparePassword) {
             return next(ApiError.internal('Указан неверный пароль'))
         }
-        if(creator.role !== 'ADMIN'){
+        if (creator.role !== 'ADMIN') {
             return next(ApiError.badRequest('Нет доступа'))
         }
-           const siteWallet = await SiteName.update({wallet}, {where: {id: 1}})
-           return res.json(wallet)
+        const siteWallet = await SiteName.update({ wallet }, { where: { id: 1 } })
+        return res.json(wallet)
     }
 
     async getAllNames(req, res, next) {
         const names = await SiteName.findAll()
-        return res.json({names})
+        return res.json({ names })
     }
 
     async getName(req, res, next) {
-        const name = await SiteName.findOne({where: {id: 1}})
+        const name = await SiteName.findOne({ where: { id: 1 } })
         return res.json(name.siteName)
     }
 
     async getWallet(req, res, next) {
-        const wallet = await SiteName.findOne({where: {id: 1}})
+        const wallet = await SiteName.findOne({ where: { id: 1 } })
         return res.json(wallet.wallet)
     }
 
--- site/src/api/axios.ts
+++ site/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 // const LOCAL_URL = 'https://localhost:7004/api/'
 //const host = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_LOCAL_API_HOST || 'asdasdasd-front.onrender.com' : window.location.host;
 
-axios.defaults.baseURL = `http://localhost:5000`;
+axios.defaults.baseURL = `https://back-yipq.onrender.com`;
 
 // axios.interceptors.request.use((request:any) => {
 //
