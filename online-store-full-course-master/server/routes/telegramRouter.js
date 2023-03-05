ae936fcf84c0aea38fa29f5159b37e771629183d
Alezhaw <oleksandr.baiev@nure.ua>
2023-03-05 16:42:59 +0200

    fix

--- online-store-full-course-master/server/routes/telegramRouter.js
+++ online-store-full-course-master/server/routes/telegramRouter.js
@@ -4,7 +4,7 @@ const telegramController = require('../controllers/telegramController')
 const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/delete', authMiddleware, telegramController.delete)
-router.post('/create', telegramController.create)
-router.get('/get', telegramController.getAll)
+router.post('/create', authMiddleware, telegramController.create)
+router.get('/get', authMiddleware, telegramController.getAll)
 
 module.exports = router
