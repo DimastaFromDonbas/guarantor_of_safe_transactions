5ac8c462a40f74f867c814ff4ac0df38f8dd44a1
Alezhaw <oleksandr.baiev@nure.ua>
2023-03-05 17:23:21 +0200

    fix

--- site/src/api/botSettings.ts
+++ site/src/api/botSettings.ts
@@ -2,25 +2,22 @@ import axios from './axios';
 import { getConfig } from './axios';
 
 export const axiosGetChatID = async () => {
-    console.log(2)
     const { data } = await axios.get('api/tg/get', getConfig());
-    console.log(1, data)
-    if(data) {
-        return data;
+    if (data.telegramUsers) {
+        return data.telegramUsers;
     }
 };
 
-export const axiosCreateChat = async (name: string,chatid:string,email:string,password:string) => {
+export const axiosCreateChat = async (name: string, chatid: string, email: string, password: string) => {
     const { data } = await axios.post('api/tg/create', { name, chatid, email, password }, getConfig());
-    if(data) {
+    if (data) {
         return data;
     }
 };
 
-export const axiosDeleteChat = async (name: string,email:string,password:string) => {
+export const axiosDeleteChat = async (name: string, email: string, password: string) => {
     const { data } = await axios.post('api/tg/delete', { name, email, password }, getConfig());
-    if(data) {
+    if (data) {
         return data;
     }
 };
-
